878e0ca239bef827459dc9c2226e83c76aabbe4b
swagenough <151718487+swagenough@users.noreply.github.com>
2025-02-01 21:17:08 +0700

    feat: add sample account data insertion to the database

--- index.js
+++ index.js
@@ -16,16 +16,34 @@ const connectToDatabase = async () => {
         console.error(`Error connecting to the database: ${err}`)
     }
 }
+const sampleAccounts = [
+    {
+        account_id: "MDB011235813",
+        account_holder: "Ada Lovelace",
+        account_type: "checking",
+        balance: 60218,
+        last_updated: new Date(),
+    },
+    {
+        account_id: "MDB829000001",
+        account_holder: "Muhammad ibn Musa al-Khwarizmi",
+        account_type: "savings",
+        balance: 267914296,
+        last_updated: new Date(),
+    },
+    ];
 
-const main = async () => {
+    const main = async () => {
     try {
-        await connectToDatabase()
+        await connectToDatabase();
+        let result = await accountsCollection.insertOne(sampleAccounts)
+        console.log(`Inserted ${result.insertedCount} documents`);
+        console.log(result);
     } catch (err) {
-        console.error(`Error connecting to the database: ${err}`)
+        console.error(`Error inserting documents: ${err}`);
     } finally {
-        await client.close()
+        await client.close();
     }
-}
-
-console.log("Hello guys")
-main()
+    };
+    
+    main()
