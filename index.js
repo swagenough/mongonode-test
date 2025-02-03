4f1b759c99ee6a33424cd87885baf07832167f76
swagenough <151718487+swagenough@users.noreply.github.com>
2025-02-04 00:25:53 +0700

    feat: update database connection and implement aggregation pipeline for account balances

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import { MongoClient } from 'mongodb'
 
 dotenv.config()
 
-const client = new MongoClient(process.env.uri, {
+const client = new MongoClient(process.env.URI, {
     serverSelectionTimeoutMS: 10000
 })
-const dbname = "feryNodejs"
+
+const dbname = "test"
+const collection_name = "accounts"
+
+const accountsCollection = client.db(dbname).collection(collection_name)
 
 const connectToDatabase = async () => {
     try {
@@ -16,34 +20,52 @@ const connectToDatabase = async () => {
         console.error(`Error connecting to the database: ${err}`)
     }
 }
+
 const sampleAccounts = [
     {
-        account_id: "MDB011235813",
-        account_holder: "Ada Lovelace",
+        account_id: "MDB011235214",
+        account_holder: "Swag Fery2",
         account_type: "checking",
         balance: 60218,
         last_updated: new Date(),
     },
     {
-        account_id: "MDB829000001",
-        account_holder: "Muhammad ibn Musa al-Khwarizmi",
+        account_id: "MDB829726122",
+        account_holder: "Lol man2",
         account_type: "savings",
         balance: 267914296,
         last_updated: new Date(),
     },
-    ];
+];
 
-    const main = async () => {
-    try {
-        await connectToDatabase();
-        let result = await accountsCollection.insertOne(sampleAccounts)
-        console.log(`Inserted ${result.insertedCount} documents`);
-        console.log(result);
-    } catch (err) {
-        console.error(`Error inserting documents: ${err}`);
-    } finally {
-        await client.close();
+const pipeline = [
+    { $match: { balance: { $gt: 70000 } } },
+    {
+        $group: {
+            _id: "$account_type",
+            total_balance: { $sum: "$balance" },
+            avg_balance: { $avg: "$balance" }
+        },
+    },
+]
+
+const main = async () => {
+try {
+    await connectToDatabase();
+    //let result = await accountsCollection.insertMany(sampleAccounts)
+    //console.log(`Inserted ${result.insertedCount} documents`);
+    //console.log(result)
+
+    let aggregateResult = accountsCollection.aggregate(pipeline)
+
+    for await (const doc of aggregateResult) {
+        console.log(doc)
     }
-    };
-    
-    main()
+} catch (err) {
+    console.error(`Error inserting documents: ${err}`);
+} finally {
+    await client.close();
+}
+};
+
+main()
